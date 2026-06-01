9915233585f991a1761b07b740a092a9604770a0
zukarinkata <jamalnademi@gmail.com>
2026-06-01 16:48:31 +0330

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php if (!defined('ACCESS_CHECK')) { die('Direct access not allowed'); } ?>
+<footer class="modern-footer-simple">
+    <div class="footer-content-simple">
+        <div class="footer-item">
+            <i class="fas fa-phone-alt fa-2x"></i>
+            <div class="footer-text">
+                <span>021-2912-2970</span>
+                <span>021-2912-2971</span>
+            </div>
+        </div>
+        <div class="footer-divider"></div>
+        <div class="footer-item">
+            <i class="fas fa-credit-card fa-2x"></i>
+            <div class="footer-text">
+                <span dir="ltr">6219-8670-0011-1936</span>
+            </div>
+        </div>
+        <div class="footer-divider"></div>
+        <div class="footer-item">
+            <i class="fas fa-copyright fa-2x"></i>
+            <div class="footer-text">
+                <span>designed by J.Na</span>
+            </div>
+        </div>
+    </div>
+</footer>
